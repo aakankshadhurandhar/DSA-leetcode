7bab2ae7573a006ada4544ca23624e2cbc98a688
Aakanksha Dhurandhar <aakankshadhurandhar34@gmail.com>
2024-08-22 20:52:05 +0530

    Time: 102 ms (22.47%) | Memory: 13.2 MB (62.63%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -4,13 +4,11 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        l,r=0,len(height)-1
         leftmax,rightmax=0,0
+        l,r=0,len(height)-1
         result=0
-
         while l < r:
-            leftmax=max(leftmax,height[l])
-            rightmax=max(rightmax,height[r])
+            leftmax,rightmax=max(leftmax,height[l]),max(rightmax,height[r])
             if height[l]<height[r]:
                 result+=leftmax-height[l]
                 l+=1
@@ -18,5 +16,9 @@ class Solution(object):
                 result+=rightmax-height[r]
                 r-=1
         return result
+            
+
+             
+
 
         
