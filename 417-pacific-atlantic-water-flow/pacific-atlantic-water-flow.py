405a358f81fcfa7149a885c321340c092809c9df
Aakanksha Dhurandhar <aakankshadhurandhar34@gmail.com>
2024-11-06 20:21:50 +0530

    Time: 746 ms (5.35%) | Memory: 13 MB (94.16%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,51 +1,69 @@
-from collections import deque
-
 class Solution(object):
-    def bfs(self, visited, queue, row, col, heights):
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    def bfs(self,visited,queue,row,col,heights):
+        directions=[(-1,0),(1,0),(0,1),(0,-1)]
         while queue:
-            x, y = queue.popleft()
-            for r, c in directions:
-                nr, nc = x + r, y + c
-                if 0 <= nr < row and 0 <= nc < col and not visited[nr][nc] and heights[nr][nc] >= heights[x][y]:
-                    visited[nr][nc] = 1
-                    queue.append((nr, nc))
+            x,y=queue.popleft()
+            for r,c in directions:
+                nr,nc=x+r,y+c
+                if 0<=nr<row and 0<=nc<col and not visited[nr][nc] and heights[nr][nc]>=heights[x][y]:
+                    visited[nr][nc]=1
+                    queue.append((nr,nc))
+
 
     def pacificAtlantic(self, heights):
         """
         :type heights: List[List[int]]
         :rtype: List[List[int]]
         """
+        # visited set for both pacific and atlantic 
+        # queue for both
+        # left 
+
         if not heights:
             return []
+        row,col=len(heights),len(heights[0])
+        visited_pacific=[[0 for _ in range(col)] for _ in range(row)]
+        visited_atlantic=[[0 for _ in range(col)] for _ in range(row)]
+        pacific_queue=deque()
+        atlantic_queue=deque()
 
-        row, col = len(heights), len(heights[0])
-        visited_pacific = [[0 for _ in range(col)] for _ in range(row)]
-        visited_atlantic = [[0 for _ in range(col)] for _ in range(row)]
-        queue_pacific = deque()
-        queue_atlantic = deque()
-        res = []
-
-        # Vertical left and right border
-        for i in range(row):
-            queue_pacific.append((i, 0))
-            queue_atlantic.append((i, col - 1))
-            visited_pacific[i][0] = 1
-            visited_atlantic[i][col - 1] = 1
-
-        # Top and bottom border
+        # top and left border
         for j in range(col):
-            queue_pacific.append((0, j))
-            queue_atlantic.append((row - 1, j))
-            visited_pacific[0][j] = 1
-            visited_atlantic[row - 1][j] = 1
-
-        self.bfs(visited_pacific, queue_pacific, row, col, heights)
-        self.bfs(visited_atlantic, queue_atlantic, row, col, heights)
-
+            #top
+            pacific_queue.append((0,j))
+            atlantic_queue.append((row-1,j))
+            visited_pacific[0][j]=1
+            visited_atlantic[row-1][j]=1
+        
+        #left border
+        for i in range(row):
+            pacific_queue.append((i,0))
+            visited_pacific[i][0]=1
+            atlantic_queue.append((i,col-1))
+            visited_atlantic[i][col-1]=1
+        print(pacific_queue)
+        print(atlantic_queue)
+        self.bfs(visited_pacific,pacific_queue,row,col,heights)
+        self.bfs(visited_atlantic,atlantic_queue,row,col,heights)
+        res=[]
+        print(visited_atlantic)
+        print(visited_pacific)
         for i in range(row):
             for j in range(col):
                 if visited_pacific[i][j] and visited_atlantic[i][j]:
-                    res.append([i, j])
-
+                    res.append([i,j])
         return res
+
+        
+
+
+        
+
+
+
+
+
+                
+    
+        
+        
